2d7e196bd9e2e90c7fa8353a3e7d4352a9e4dd5d
Arinji2 <87296817+Arinji2@users.noreply.github.com>
2023-01-14 02:02:29 +0530

    Add Packages

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyB58DNUia7wT_W9oOF-s7l0ZHk7B7SZt8c",
+  authDomain: "arinji-9a53b.firebaseapp.com",
+  databaseURL:
+    "https://arinji-9a53b-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "arinji-9a53b",
+  storageBucket: "arinji-9a53b.appspot.com",
+  messagingSenderId: "992504505004",
+  appId: "1:992504505004:web:c99ee7b53e8d0e2b897509",
+  measurementId: "G-V41MBDNM63",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
